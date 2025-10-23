74fc229cd1c2901260542a92a2b4c44f73f5c429
Simon <omojide.o.j@gmail.com>
2025-10-23 17:13:34 +0100

    Implement Cookies in Route Handler

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,15 +1,23 @@
 import { type NextRequest } from 'next/server';
-import { headers } from 'next/headers';
+import { cookies, headers } from 'next/headers';
 
-const GET = async (_req: NextRequest) => {
+const GET = async (req: NextRequest) => {
 	// const reqHeaders = new Headers(req.headers);
 	// console.log(reqHeaders.get('Authorization'));
 
 	const newHeaders = await headers();
 	console.log(newHeaders.get('Authorization'));
 
+	const getCookie = req.cookies.get('theme');
+	console.log(getCookie);
+
+	const cookieStore = await cookies();
+	cookieStore.set('resultPage', 'Working');
+
+	console.log(cookieStore.get('resultPage'));
+
 	return new Response('<h1>Profile Api World</h1>', {
-		headers: { 'Content-Type': 'text/html' },
+		headers: { 'Content-Type': 'text/html', 'Set-Cookie': 'theme=dark' },
 	});
 };
 
