10535c0c03621849bec480b12a73bacf3f46e1a1
Simon <omojide.o.j@gmail.com>
2025-10-23 15:41:16 +0100

    Implement URL query parameters

--- src/app/comments/route.ts
+++ src/app/comments/route.ts
@@ -1,8 +1,15 @@
+import { NextRequest } from 'next/server';
 import { comments } from './data';
 
-const GET = async () => {
-	return Response.json(comments);
+const GET = async (req: NextRequest) => {
+	const searchParams = req.nextUrl.searchParams; // Grab all query parameters
+	const query = searchParams.get('query'); //fetch specific query parameter
+	const filteredComments = query
+		? comments.filter((comment) => comment.text.includes(query.toLowerCase()))
+		: comments;
+	return Response.json(filteredComments); // If query, we filter to only include search terms
 };
+
 const POST = async (request: Request) => {
 	const comment = await request.json();
 	const newComment = {
