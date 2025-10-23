72ecb27f9052c88770a0790498bcd0d46e083945
Simon <omojide.o.j@gmail.com>
2025-10-23 16:29:37 +0100

    Implement headers in route handlers

--- src/app/comments/route.ts
+++ src/app/comments/route.ts
@@ -1,4 +1,4 @@
-import { NextRequest } from 'next/server';
+import { type NextRequest } from 'next/server';
 import { comments } from './data';
 
 const GET = async (req: NextRequest) => {
--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,5 +1,16 @@
-const GET = async () => {
-	return new Response('Profile Api World');
+import { type NextRequest } from 'next/server';
+import { headers } from 'next/headers';
+
+const GET = async (_req: NextRequest) => {
+	// const reqHeaders = new Headers(req.headers);
+	// console.log(reqHeaders.get('Authorization'));
+
+	const newHeaders = await headers();
+	console.log(newHeaders.get('Authorization'));
+
+	return new Response('<h1>Profile Api World</h1>', {
+		headers: { 'Content-Type': 'text/html' },
+	});
 };
 
 export { GET };
