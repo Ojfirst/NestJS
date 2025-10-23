4f143c31714b8344bb3eeb1a3c918c59735fcdc2
Simon <omojide.o.j@gmail.com>
2025-10-23 20:59:16 +0100

    Implement full midddleware for cookies and header request

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,10 +2,20 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export const middleware = (req: NextRequest) => {
-	// return NextResponse.redirect(new URL('/', req.url));
-	if (req.nextUrl.pathname === '/profile') {
-		return NextResponse.redirect(new URL('/comments', req.nextUrl));
+	const response = NextResponse.next();
+	const themePreference = req.cookies.get('theme');
+	if (!themePreference) {
+		response.cookies.set('theme', 'dark');
 	}
+
+	response.headers.set('custom-heaader', 'custome-value');
+
+	return response;
+
+	// return NextResponse.redirect(new URL('/', req.url));
+	// if (req.nextUrl.pathname === '/profile') {
+	// 	return NextResponse.redirect(new URL('/comments', req.nextUrl));
+	// }
 };
 
 // // Config object to only apply middleware to only apply to profile route.
